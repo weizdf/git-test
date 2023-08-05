cbea394d99e47944911c0dc90238b30adce8b9d0
walyou <walyou@atguigu.com>
2023-08-05 16:07:48 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git2222!");
         System.out.println("hello git3333!");
         System.out.println("hello git4444!");
+        System.out.println("master test!");
     }
 }
